f4e9b33be434bec0b0f25172db598ed698cbb8e6
Alexa Dominguez <alexadominguez@Alexas-MacBook-Pro.local>
2020-10-14 22:22:02 -0500

    removed required props

--- sticky_notes_backend/models/Post.js
+++ sticky_notes_backend/models/Post.js
@@ -3,11 +3,9 @@ const mongoose = require("mongoose");
 const PostSchema = mongoose.Schema({
   title: {
     type: String,
-    required: true,
   },
   description: {
     type: String,
-    required: true,
   },
   date: {
     type: Date,
